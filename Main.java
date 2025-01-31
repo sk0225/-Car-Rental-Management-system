a5b1e3820869a216e831a527d980b03211a7ba74
sk0225 <157195094+sk0225@users.noreply.github.com>
2025-01-31 09:59:57 +0530

    Update Main.java
    change in the main class

--- Main.java
+++ Main.java
@@ -243,7 +243,7 @@ public class Main{
     public static void main(String[] args) {
         CarRentalSystem rentalSystem = new CarRentalSystem();
 
-        Car car1 = new Car("C001", "Toyota", "Camry", 60.0); // Different base price per day for each car
+        Car car1 = new Car("C001", "Toyota", "Camry", 60.0); 
         Car car2 = new Car("C002", "Honda", "Accord", 70.0);
         Car car3 = new Car("C003", "Mahindra", "Thar", 150.0);
         rentalSystem.addCar(car1);
